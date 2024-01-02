6ddc88d5154c3f884a246ea080999aada6305971
Younghyun Lee <younghyunlee22@gmail.com>
2024-01-02 14:44:17 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
+}
